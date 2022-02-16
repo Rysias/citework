6cd3765757921877dbe37cb86e6dfb57b9d17e23
rysias <jvhithamar@gmail.com>
2022-02-16 13:51:48 +0100

    specify function

--- R/extract_doi.R
+++ R/extract_doi.R
@@ -1,6 +1,4 @@
-library(stringr)
-
 extract_doi <- function(cite_string) {
   doi_pattern <- "(?<=https://doi.org/)(.+)"
-  str_extract(cite_string, doi_pattern)
+  stringr::str_extract(cite_string, doi_pattern)
 }
