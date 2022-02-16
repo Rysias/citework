5cab9d560e68f3baf1dbf7322cb632057569d81d
rysias <jvhithamar@gmail.com>
2022-02-16 13:36:37 +0100

    upgrade description + add NA test case

--- tests/testthat/test_doi_extract.R
+++ tests/testthat/test_doi_extract.R
@@ -1,5 +1,4 @@
 library(citework)
-library(testthat)
 
 test_that("extracting DOI single", {
   cite_string <- "Ramscar, M., Dye, M., & Klein, J. (2013). Children Value Informativity Over Logic in Word Learning. Psychological Science, 24(6), 1017–1023. https://doi.org/10.1177/0956797612460691"
@@ -11,4 +10,8 @@ test_that("extracting DOI vector", {
   cite_string2 <- "Aravind, A., de Villiers, J., Pace, A., Valentine, H., Golinkoff, R., Hirsh-Pasek, K., … Sweig Wilson, M. (2018). Fast mapping word meanings across trials: Young children forget all but their first guess. Cognition, 177(April), 177–188. https://doi.org/10.1016/j.cognition.2018.04.008"
   cite_vec <- c(cite_string1, cite_string2)
   expect_equal(extract_doi(cite_vec), c("10.1177/0956797612460691","10.1016/j.cognition.2018.04.008"))
+})
+
+test_that("Throws NA at no match", {
+  expect_true(is.na(extract_doi("No string here doi:dslgkj")))
 })
