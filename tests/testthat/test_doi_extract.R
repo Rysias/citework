c5ed60eef1aad2e9888e09a1be61b443cba519ad
rysias <jvhithamar@gmail.com>
2022-03-11 10:12:22 +0100

    add tricky test case

--- tests/testthat/test_doi_extract.R
+++ tests/testthat/test_doi_extract.R
@@ -5,6 +5,14 @@ test_that("extracting DOI single", {
   expect_equal(extract_doi(cite_string), "10.1177/0956797612460691")
 })
 
+test_that("extracting tricky DOI", {
+  cite_string <- "Wang, Y. & Seidl, A.  (2014). The Learnability of Phonotactic Patterns in Onset & Coda Positions, Language Learning & Development, DOI: 10.1080/15475441.2013.876270"
+  expect_equal(extract_doi(cite_string), "10.1080/15475441.2013.876270")
+})
+
+""
+
+
 test_that("extracting DOI vector", {
   cite_string1 <- "Ramscar, M., Dye, M., & Klein, J. (2013). Children Value Informativity Over Logic in Word Learning. Psychological Science, 24(6), 1017–1023. https://doi.org/10.1177/0956797612460691"
   cite_string2 <- "Aravind, A., de Villiers, J., Pace, A., Valentine, H., Golinkoff, R., Hirsh-Pasek, K., … Sweig Wilson, M. (2018). Fast mapping word meanings across trials: Young children forget all but their first guess. Cognition, 177(April), 177–188. https://doi.org/10.1016/j.cognition.2018.04.008"
